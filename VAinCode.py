d88e1a0ce2b2b77081731a0b40888e803a87081d
Adauto Neto <adauto.ec@gmail.com>
2020-09-28 13:43:43 -0400

    funcao que cacula quanntos elementos encontramos no xml

--- VAinCode.py
+++ VAinCode.py
@@ -2,9 +2,6 @@ import os
 from xml.dom import minidom
 
 
-elementos = ['TextView', 'LinearLayout', 'RelativeLayout', 'GridLayout', 'Button', 'ImageButton', 'CheckedBox', 'EditText', 'ImageView']
-
-
 def percorrePasta(pasta):
 	caminhos = [os.path.join(pasta, nome) for nome in os.listdir(pasta)]
 	pastas = [pas for pas in caminhos if os.path.isdir(pas)]
@@ -19,18 +16,34 @@ def filtro(arquivos, extencao):
 
 
 def attsDoElemento(arquivo, elementos):
+	attributosDoArquivo = []
 	for elemento in elementos:
 		arqXml = minidom.parse(arquivo)
 		items = arqXml.getElementsByTagName(elemento)
 		for item in items:
-			print(item.attributes.keys())
-	    
+			attributosDoArquivo = attributosDoArquivo + list(item.attributes.keys())
+	return attributosDoArquivo
+
+
+def attsProcurados(atributosDoElemento, atributosProcurados):
+	total = 0
+	procurado = 0
+	for attp in atributosProcurados:
+		for atte in atributosDoElemento:
+			if attp in atte: 
+				procurado = procurado+1
+			total = total+1
+	return 100*procurado/total if (total != 0) else 0
+		
 
 
 def main():
-    arquivosXml = filtro(percorrePasta('./'), '.xml')
-    for arquivo in arquivosXml:
-    	attsDoElemento(arquivo, elementos)    
+	elementos = ['TextView', 'LinearLayout', 'RelativeLayout', 'FrameLayout', 'GridLayout', 'Button', 'ImageButton', 'CheckedBox', 'EditText', 'ImageView', 'CartView', 'BrowserView']
+	atributos = [':contetDescription', ':importantForAccessibility', ':id', ':accessibilityPaneTitle']
+
+	arquivosXml = filtro(percorrePasta('./'), '.xml')
+	for arquivo in arquivosXml:
+		print(attsProcurados(attsDoElemento(arquivo, elementos), atributos))
 
 
 if __name__ == "__main__":
